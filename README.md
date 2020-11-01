ba8ef816202e8adae48df96e237deb60ed1c3262
chendapeng <291003932@qq.com>
2020-11-01 22:21:40 +0800

    新功能(脚手架): ::rocket 初建模型

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-## cpp-cli
+## cpp 
+自我练习的脚手架
+
+### 删除文件夹
+`cpp rm <dir>`
+
+### 升级模板
+*cpp upgrade*
+
+### 设置自己镜像
+*cpp mirror <template_mirror>*
+
+### 下载模板
+*cpp template*
+
+### 项目初始化
+*cpp init <project_name>*
+
+### 接下来需要做的事
+- 动态修改模板里的*package.json*里的name和作者
+- 运用webpack搭建自己的需要的模板
+- 完善*cpp-cli-test*脚手架里的命令行和帮助文档
--- bin/index.js
+++ bin/index.js
@@ -2,20 +2,63 @@
 
 // commander 实现 NodeJS 命令行）
 const program = require('commander')
-const updateCheck = require('./../lib/update')
-console.log('updateCheck', updateCheck);
+const setMirror = require('../lib/mirror')
+const updateChk = require('../lib/update')
+// 请求 lib/download.js
+const dlTemplate = require('../lib/download')
+// 项目初始化
+const initProject = require('../lib/init')
+
+// 删除文件夹
+const deleteDir = require('../lib/delete')
 // version
 program.version(
-  require('../package.json').version, '-v, --version'
+  require('../package.json').version, '-V, --version'
 )
-// upgrate
+// 删除文件
 program
+  .command('rm <dir>')
+  .option('-r, --recursive', 'Remove recursively')
+  .action(function (dir) {
+    deleteDir(dir)
+  });
+
+program
+    // 声明的命令
     .command('upgrade')
-    .description('check Cpp-cli version')
+    .option('-u, --upgrade', 'upgrade version')
+    // 描述信息，在帮助信息时显示
+    .description("Check the cpp-cli-test version.")
     .action(() => {
-      updateCheck()
+      // 执行 lib/update.js 里面的操作
+      updateChk()
     })
 
+program
+    .command('mirror <template_mirror>')
+    .description('设置自己的镜像模板')
+    .action((tpl) => {
+      console.log('tpl', tpl);
+      setMirror(tpl)
+    })
+
+// 远程下载自己的模板
+program
+    .command('template')
+    .description('Download template from mirror.')
+    .action(() => {
+      dlTemplate()
+    })
+
+// 项目初始化
+program
+    .name('cpp')
+    .usage('<commands> [options]')
+    .command('init <project_name>')
+    .description('项目初始化')
+    .action((project) => {
+      initProject(project)
+    })
 
 // 解析命令行参数
 program.parse(process.argv)
